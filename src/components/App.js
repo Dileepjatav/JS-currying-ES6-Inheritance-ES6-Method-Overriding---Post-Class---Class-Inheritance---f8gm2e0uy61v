57995ba10a0651d4e51783a1a93a993b4eddb70f
ROSHAN KUMAR JHA <73186190+roshan837@users.noreply.github.com>
2022-06-24 20:45:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import './circle.js';
+import './shape.js';
 const App = () => {
   return (
     <div id="main"></div>
